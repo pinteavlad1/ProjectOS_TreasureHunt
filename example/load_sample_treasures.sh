a97a056ae8a635684e707f468a0a5d38773e7072
Pintea Vlad <pinteavladc@gmail.com>
2025-05-25 10:46:39 +0300

    Added example with sample data

---
+++ example/load_sample_treasures.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ../bin
+
+./treasure_manager --add 'Hunt1' > /dev/null
+./treasure_manager --add 'Hunt1' > /dev/null
+./treasure_manager --add 'Hunt1' > /dev/null
+./treasure_manager --add 'Hunt2' > /dev/null
+./treasure_manager --add 'Hunt2' > /dev/null
+./treasure_manager --list 'Hunt1'
